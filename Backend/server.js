3e083003e362622b5bb9ba0727f766c7550ca4cd
moyellamahitha <moyellamahitha123@gmail.com>
2025-06-20 17:12:42 +0530

    new

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3006;
+const port = 3052;
 
 // Middleware
 app.use(cors());
